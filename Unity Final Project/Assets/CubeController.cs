119cd5c575dc0fbc0885d41e3915013ae15ae922
BerryhillWilliA9251 <BerryhillWilliA9251@sduhsd.lan>
2018-01-10 11:49:43 -0800

    iuawyehoi

---
+++ Unity Final Project/Assets/CubeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = new Vector3(0, 6, 6);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+
+}
