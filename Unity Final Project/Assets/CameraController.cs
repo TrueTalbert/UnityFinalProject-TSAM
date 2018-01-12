4de3aa5fe03e83f9120e442eb77ade9f0cd2ef45
BerryhillWilliA9251 <BerryhillWilliA9251@sduhsd.lan>
2018-01-12 10:35:12 -0800

    weird camera

---
+++ Unity Final Project/Assets/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public float sens;
+	public bool hideCursor;
+
+	// Use this for initialization
+	void Start () {
+		if (hideCursor) {
+			Cursor.visible = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotY = transform.localEulerAngles.y + Input.GetAxis ("Mouse X") * sens;
+		float rotX = transform.localEulerAngles.x - Input.GetAxis ("Mouse Y") * sens;
+		gameObject.transform.localEulerAngles = new Vector3 (rotX, rotY, 0);
+	}
+}
